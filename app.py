fb864612a1d981a5dfeb9a7ea1b6c3bad12bf1ad
Rahmaaa23 <nuzuliarahmatunnisa@gmail.com>
2025-07-12 09:26:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,14 @@ with st.sidebar:
         time.sleep(5)
     st.success("Done!")
 
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
+with tab1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+with tab2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+with tab3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
