3eca1b1937032ff599c63627f4e3cab3907e3a0e
Rahmaaa23 <nuzuliarahmatunnisa@gmail.com>
2025-07-12 09:20:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,17 @@ st.markdown("*Streamlit* is **really** ***cool***.")
 
 st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
