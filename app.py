480967d15d0ec8cff602e42d3f6f542e152530d5
Rahmaaa23 <nuzuliarahmatunnisa@gmail.com>
2025-07-12 09:16:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ st.header("_Streamlit_ is :blue[cool] :sunglasses:")
 st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
 
 st.markdown("*Streamlit* is **really** ***cool***.")
+
+st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+
